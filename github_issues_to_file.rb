29c2f0da884407c2af3acf3284d1ae33b64b4324
Kazuhiro Homma <kazu.homma@gmail.com>
2015-05-25 18:08:44 +0900

    自分用に改造

--- github_issues_to_file.rb
+++ github_issues_to_file.rb
@@ -4,15 +4,13 @@
 # https://gist.github.com/tkarpinski/2369729
 
 require 'pp'
+require 'pry'
 require 'octokit'
-require 'pstore'
 require 'yaml'
 
 # 設定ファイル読み出し
 if File.exists?("info.yml")
   CONFIG = YAML.load_file("info.yml")
-else
-  CONFIG = YAML.load_file("info_sample.yml")
 end
 
 # Github credentials to access your private project
@@ -26,36 +24,29 @@ PROJECT = CONFIG["project"]
 # Your local timezone offset to convert times
 TIMEZONE_OFFSET="0"
 
-client = Octokit::Client.new(:login => USERNAME, :password => PASSWORD)
+Octokit.auto_paginate = true
 
-# 保存先ファイル
-db = PStore.new("#{PROJECT}_issues.db")
-
-# open, close 両方のissueを全て取得
-puts "Getting issues from Github..."
-temp_issues = []
-issues = []
-page = 0
-begin
-  page = page +1
-  temp_issues = client.list_issues("#{USER}/#{PROJECT}", :state => "closed", :page => page)
-  issues = issues + temp_issues;
-end while not temp_issues.empty?
-temp_issues = []
-page = 0
-begin
-  page = page +1
-  temp_issues = client.list_issues("#{USER}/#{PROJECT}", :state => "open", :page => page)
-  issues = issues + temp_issues;
-end while not temp_issues.empty?
-
-# 取得したissue をシリアライズ保存
-puts "Processing #{issues.size} issues..."
-issues.each do |issue|
-  puts "Processing issue #{issue['number']}..."
-
-  # ハッシュと同じようにキーを指定して保存
-  db.transaction do
-    db["issue_#{issue['number']}"] = issue
-  end # ここで保存される
+def repo
+  "#{USER}/#{PROJECT}"
 end
+
+def client
+  @client = Octokit::Client.new(:login => USERNAME, :password => PASSWORD)
+end
+
+def milestone_number(name)
+  @client.list_milestones(repo).select {|milestone|
+    milestone.title.include?(name)
+  }.first.number
+end
+
+def issues(options = {})
+  client.list_issues(repo, :state => "open", milestone: milestone_number(options[:milestone]))
+end
+
+milestone_name = ARGV[0]
+
+puts issues(milestone: milestone_name).map {|i|
+  "#{Time.parse(i.updated_at)} -- #{i.title}"
+}.sort
+
